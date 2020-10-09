87fd8d910ef0108aadaaf3badb43ae394155714d
faqster <faqster@gmail.com>
2020-10-09 22:25:13 +0200

    Cleaning up

--- intonation_synthesis/utils.py
+++ intonation_synthesis/utils.py
@@ -1,6 +1,7 @@
 import numpy
 import os
 import pandas
+import tensorflow as tf
 
 from datetime import datetime
 
@@ -52,3 +53,57 @@ def scale(value, old_min, old_max, new_min=0, new_max=1):
     old_range = (old_max - old_min)
     new_range = (new_max - new_min)
     return (((value - old_min) * new_range) / old_range) + new_min
+
+
+def _get_layer_of_nth_path_in_residual_block(res_block, n, layer_name):
+    results = sorted(
+        [
+            (layer, layer.name) for layer in res_block.layers
+            if layer_name in layer.name
+        ],
+        key=lambda x: int(
+            x[0].name[x[0].name.rfind("_") + 1:]
+            if x[0].name[x[0].name.rfind("_") + 1:].isnumeric() else 0))
+
+    print(results[0][1])
+    return results[0][0]
+
+def list_nested_conv_layers(layers_list, skip_suffix=None):
+    parsed_layers = []
+    for layer in layers_list:
+        if not isinstance(layer, tf.keras.layers.Layer):
+            continue
+        elif "conv" in layer.name:
+            if skip_suffix is not None:
+                if layer.name.endswith(skip_suffix):
+                    continue
+            filters, biases = layer.get_weights()
+            print("{}: filters: {}".format(layer.name, filters.shape))
+            parsed_layers.append(layer)
+        elif "residual" in layer.name:
+            print("Residual Block: {}:".format(layer.name))
+            conv = _get_layer_of_nth_path_in_residual_block(
+                layer, 0, "conv1D")
+            norm = _get_layer_of_nth_path_in_residual_block(
+                layer, 0, "batch_normalization")
+            activation = _get_layer_of_nth_path_in_residual_block(
+                layer, 0, "activation")
+            dropout = _get_layer_of_nth_path_in_residual_block(
+                layer, 0, "spatial_dropout1d")
+            residual_layers_first_path = [conv, norm, activation, dropout]
+            parsed_layers.extend(residual_layers_first_path)
+        elif "lambda" in layer.name:
+            continue
+        else:
+            continue
+            print("Layer: {} (skipping)".format(layer.name))
+
+    return parsed_layers
+
+
+def get_tcn_model_layers(model):
+    tcn_layer = model.layers[0]
+    # camelCase notation copied from the TSViz-Core library
+    outputLayers = list_nested_conv_layers(tcn_layer._layers)
+    outputLayers.extend(model.layers[1:])
+    return outputLayers
